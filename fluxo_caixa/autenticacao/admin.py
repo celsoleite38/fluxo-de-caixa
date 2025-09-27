f533b6e4318de2a2c355083ed21897e53203750f
JOSE CELSO CARVALHO LEITE <79555971+celsoleite38@users.noreply.github.com>
2025-09-27 13:46:29 -0300

    Delete fluxo_caixa/autenticacao directory

--- fluxo_caixa/autenticacao/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Ativacao
-
-admin.site.register(Ativacao)
--- fluxo_caixa/autenticacao/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import PerfilProfissional
-
-def perfil_profissional(request):
-    if request.user.is_authenticated:
-        perfil = PerfilProfissional.objects.filter(usuario=request.user).first()
-        return {'perfil': perfil}
-    return {'perfil': None}
